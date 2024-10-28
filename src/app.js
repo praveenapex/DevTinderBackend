5777ab020f34b7156176f826de3be22c91b240a8
praveenmarch22 <praveen.220302@gmail.com>
2024-10-28 23:16:10 +0530

    added validations

--- src/app.js
+++ src/app.js
@@ -17,6 +17,72 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+app.get("/getUser", async (req, res) => {
+  try {
+    const user = await User.find({ emailId: req.body.email });
+    if (user.length !== 0) {
+      res.send(user);
+    } else {
+      res.status(404).send("User not found");
+    }
+  } catch (error) {
+    console.log(error);
+    res.send("Error while getting a user");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const user = await User.find();
+    if (user.length !== 0) {
+      res.send(user);
+    } else {
+      res.status(404).send("Users not found");
+    }
+  } catch (error) {
+    console.log(error);
+    res.send("Error while getting a users");
+  }
+});
+
+app.delete("/deleteUser", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.body.id);
+    res.send("User deleted successfully");
+  } catch (error) {
+    console.log(error);
+    res.send("Internal server error");
+  }
+});
+
+app.patch("/updateUser/:userId", async (req, res) => {
+  try {
+    const ALLOWED_UPDATES = [
+      "firstName",
+      "lastName",
+      "password",
+      "age",
+      "gender",
+      "about",
+      "skills",
+      "photoUrl",
+    ];
+    const isValidated = Object.keys(req.body).every((k) =>
+      ALLOWED_UPDATES.includes(k)
+    );
+    if (!isValidated) {
+      throw new Error("Cannot update the fields");
+    }
+    const user = await User.findByIdAndUpdate(req.params.userId, req.body, {
+      runValidators: true,
+    });
+    res.send("User data is updated successfully");
+  } catch (error) {
+    console.log(error);
+    res.send("Cannot update the fields" + error.message);
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("Database connection is established..");
@@ -24,6 +90,6 @@ connectDB()
       console.log("Server is running on ", PORT);
     });
   })
-  .catch(() => {
-    console.log("Error occurred while connecting to the database...");
+  .catch((error) => {
+    console.log("Error occurred while connecting to the database...", error);
   });
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,60 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate: (v) => {
+        if (!validator.isEmail(v)) {
+          throw new Error("Enter a Valid email");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value.toLowerCase())) {
+          throw new Error("Enter valid gender");
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: "This is a default about..",
+    },
+    skills: {
+      type: [String],
+    },
+    photo: {
+      type: String,
+      default:
+        "https://m.media-amazon.com/images/I/71JpPdKSEAL._AC_UY1100_.jpg",
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
