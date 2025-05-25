18ae8d9f0b6dde6aff0d40981b3a02e92611793f
praveenmarch22 <praveen.220302@gmail.com>
2025-05-25 12:11:23 +0530

    chnaged the code

--- src/router/auth.js
+++ src/router/auth.js
@@ -4,6 +4,7 @@ const User = require("../models/user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
+
 const authRouter = express.Router();
 
 authRouter.post("/signup", async (req, res) => {
